ae4a03ddb33dc99eee9418c7f933d0ef6dcac3da
bhupesh-abjima <97227019+bhupesh-abjima@users.noreply.github.com>
2022-02-10 19:00:49 +0530

    Add files via upload

---
+++ MyClasses.Designer.cs
@@ -0,0 +1,16 @@
+﻿namespace MyClassesTest {
+    
+    
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    internal sealed partial class MyClasses : global::System.Configuration.ApplicationSettingsBase {
+        
+        private static MyClasses defaultInstance = ((MyClasses)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new MyClasses())));
+        
+        public static MyClasses Default {
+            get {
+                return defaultInstance;
+            }
+        }
+    }
+}
